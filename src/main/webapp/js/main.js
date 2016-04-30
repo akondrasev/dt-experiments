1bc82b6552bf417b81fd4adbaa70ad5bda7580c6
Anton <anton.kondrasev@live.com>
2016-04-30 22:14:28 +0300

    last

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,23 +1,49 @@
+$.fn.dataTableExt.aoFeatures.push({
+	fnInit  : function (oSettings) {
+		var startTime = new Date().getTime();
+		var row = $("<tr>");
+
+		//console.log(oSettings.aoColumns);
+		for (var i = 0; i < oSettings.aoColumns.length; i++){
+			row.append("<td><input type='text'/></td>");
+		}
+
+		var endTime = new Date().getTime();
+
+		console.log((endTime - startTime));
+		return null;
+	},
+	cFeature: "C",
+	sFeature: "fff"
+});
+console.log($.fn.dataTableExt.aoFeatures);
 $("table").dataTable({
-    data        : staticData,
-    columns     : [
-        {"data": "partyName", title: "Name", width: 200, type: "text"},
-        {"data": "partyAddress", title: "Address", width: 200, type: "text"},
-        {"data": "partyDate", title: "Date", width: 100, type: "text"},
-        {"data": "partyId", title: "Delete", width: 100, type: "text"}
-    ],
-    dom         : '<"table-container"rtZ>',
-    colResize   : {
-        tableWidthFixed: false
-    },
-    fixedColumns: false,
-    colReorder  : false,
-    autoWidth   : true,
-    scrollX     : "100%",
-    scrollY     : "150px",
-    scrollCollapse: true,
-    select      : {
-        info : false,
-        style: "os"
-    }
+	data          : staticData,
+	columns       : [
+		{"data": "partyName", title: "Name", width: 200, type: "text"},
+		{"data": "partyAddress", title: "Address", width: 200, type: "text"},
+		{"data": "partyDate", title: "Date", width: 100, type: "text"},
+		{"data": "partyId", title: "Delete", width: 100, type: "text"}
+	],
+	dom           : 'ZC<"table-container"rtlp>',
+	colResize     : {
+		tableWidthFixed: false
+		//exclude        : [0]
+	},
+	fixedColumns  : false,
+	colReorder    : true,
+	autoWidth     : true,
+	scrollX       : "250px",
+	scrollY       : "200px",
+	scrollCollapse: true,
+	select        : {
+		blurable : true,
+		className: "selected",
+		info     : false,
+		//items    : "rows",
+		//selector : "td, th",
+		style    : "os"
+	},
+	initComplete: function () {
+	}
 });
